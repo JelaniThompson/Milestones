35545263e999cc133848a2e0da3cd5e965079b88
Jelani Thompson <jelani.thompson@outlook.com>
2017-03-21 19:51:11 -0400

    State functions

--- Date.cpp
+++ Date.cpp
@@ -7,47 +7,55 @@ using namespace std;
 #include "POS.h"
 
 // Draw a diagram of the problem
-namespace ict{
+namespace ict {
 
 	// Constructors
 	Date::Date() {
 		m_dateOnly = false;
 		set();
 	}
-	Date::Date() {
 
+	Date::Date(int year, int mon, int day) {
+		m_dateOnly = true;
+		m_year = year;
+		m_mon = mon;
+		m_day = day;
+		m_hour = 0;
+		m_min = 0;
 	}
-	Date::Date(int year, int mon, int day, int hour, int min) {
-		// Default value of zero so constructor works with four arguments as well
-		min = 0;
+
+	Date::Date(int year, int mon, int day, int hour, int min = 0) {
+		m_dateOnly = false;
 
 		m_year = year;
 		m_mon = mon;
 		m_day = day;
 		m_hour = hour;
 		m_min = min;
+
+		m_readErrorCode = NO_ERROR;
 	}
 
- 	void Date::errCode(int errorCode) {
+	void Date::errCode(int errorCode) {
 		switch (errorCode) {
-			case 0:
-				errorCode = NO_ERROR;
-				break;
-			case 1:
-				errorCode = CIN_FAILED;
-				break;
-			case 3:
-				errorCode = YEAR_ERROR;
-				break;
-			case 4:
-				errorCode = DAY_ERROR;
-				break;
-			case 5:
-				errorCode = HOUR_ERROR;
-				break;
-			case 6:
-				errorCode = MIN_ERROR;
-				break;
+		case 0:
+			errorCode = NO_ERROR;
+			break;
+		case 1:
+			errorCode = CIN_FAILED;
+			break;
+		case 3:
+			errorCode = YEAR_ERROR;
+			break;
+		case 4:
+			errorCode = DAY_ERROR;
+			break;
+		case 5:
+			errorCode = HOUR_ERROR;
+			break;
+		case 6:
+			errorCode = MIN_ERROR;
+			break;
 		}
 	}
 
@@ -60,59 +68,80 @@ namespace ict{
 		m_readErrorCode = NO_ERROR;
 	}
 
-	void Date::set(){
-    time_t t = time(NULL);
-    tm lt = *localtime(&t);
-    m_day = lt.tm_mday;
-    m_mon = lt.tm_mon + 1;
-    m_year = lt.tm_year + 1900;
-    if (dateOnly()){
-      m_hour = m_min = 0;
-    }
-    else{
-      m_hour = lt.tm_hour;
-      m_min = lt.tm_min;
-    }
-  }
+	void Date::set() {
+		time_t t = time(NULL);
+		tm lt = *localtime(&t);
+		m_day = lt.tm_mday;
+		m_mon = lt.tm_mon + 1;
+		m_year = lt.tm_year + 1900;
+		if (dateOnly()) {
+			m_hour = m_min = 0;
+		}
+		else {
+			m_hour = lt.tm_hour;
+			m_min = lt.tm_min;
+		}
+	}
 
-  bool Date::operator==(const Date & D) const {
-	  if (this->value() == D.value()) { return true; }
-	  else { return false; }
-  }
+	int Date::value()const {
+		return m_year * 535680 + m_mon * 44640 + m_day * 1440 + m_hour * 60 + m_min;
+	}
 
-  bool Date::operator!=(const Date & D) const {
-	  if (this->value() != D.value()) { return true; }
-	  else { return false; }
-  }
+	int Date::errCode()const {
+		return m_readErrorCode;
+	}
 
-  bool Date::operator<(const Date & D) const {
-	  if (this->value() < D.value()) { return true; }
-	  else { return false; }
-  }
+	bool Date::bad()const {
+		if (m_readErrorCode != 0) { return true; }
+		else { return false; }
+	}
 
-  bool Date::operator>(const Date & D) const {
-	  if (this->value() > D.value()) { return true; }
-	  else { return false; }
-  }
+	bool Date::dateOnly()const {
+		return m_dateOnly;
+	}
 
-  bool Date::operator<=(const Date & D) const {
-	  if (this->value() <= D.value()) { return true; }
-	  else { return false; }
-  }
+	void Date::dateOnly(bool value) {
+		m_dateOnly = value;
+		if (value == true) { m_hour = 0; m_min = 0; }
+	}
 
-  bool Date::operator>=(const Date & D) const {
-	  if (this->value() >= D.value()) { return true; }
-	  else { return false; }
-  }
+	int Date::mdays()const {
+		int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, -1 };
+		int mon = m_mon >= 1 && m_mon <= 12 ? m_mon : 13;
+		mon--;
+		return days[mon] + int((mon == 1)*((m_year % 4 == 0) && (m_year % 100 != 0)) || (m_year % 400 == 0));
+	}
 
-  int Date::value()const{
-    return m_year * 535680 + m_mon * 44640 + m_day * 1440 + m_hour * 60 + m_min;
-  }
+	// Do IO member functions here
 
-  int Date::mdays()const{
-    int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, -1 };
-    int mon = m_mon >= 1 && m_mon <= 12 ? m_mon : 13;
-    mon--;
-    return days[mon] + int((mon == 1)*((m_year % 4 == 0) && (m_year % 100 != 0)) || (m_year % 400 == 0));
-  }
+	// ---------------------------------------------------------- Operators
+	bool Date::operator==(const Date & D) const {
+		if (this->value() == D.value()) { return true; }
+		else { return false; }
+	}
+
+	bool Date::operator!=(const Date & D) const {
+		if (this->value() != D.value()) { return true; }
+		else { return false; }
+	}
+
+	bool Date::operator<(const Date & D) const {
+		if (this->value() < D.value()) { return true; }
+		else { return false; }
+	}
+
+	bool Date::operator>(const Date & D) const {
+		if (this->value() > D.value()) { return true; }
+		else { return false; }
+	}
+
+	bool Date::operator<=(const Date & D) const {
+		if (this->value() <= D.value()) { return true; }
+		else { return false; }
+	}
+
+	bool Date::operator>=(const Date & D) const {
+		if (this->value() >= D.value()) { return true; }
+		else { return false; }
+	}
 }
