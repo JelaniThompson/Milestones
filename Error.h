60f1423d62c67e37bf522b46448b9747b3608aa4
Jelani Thompson <jelani.thompson@outlook.com>
2017-03-21 19:52:31 -0400

    Initial commit

---
+++ Error.h
@@ -0,0 +1,24 @@
+// compilation safegaurds
+
+#include <iostream>
+namespace ict {
+   class Error {
+      char* m_message;
+   public:
+   // constructors
+
+   // destructor
+
+   // deleted constructor and operator=
+
+   // operator= for c-style strings
+
+   // methods
+
+   // cast overloads
+
+   };
+   // operator << overload prototype for cout
+}
+
+
