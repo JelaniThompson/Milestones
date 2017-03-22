656fdb19c83c47dcaf55f2f677f5d7b93810ef33
Jelani Thompson <jelani.thompson@outlook.com>
2017-03-21 20:10:12 -0400

    Updated Error.h

--- Error.h
+++ Error.h
@@ -6,19 +6,25 @@ namespace ict {
       char* m_message;
    public:
    // constructors
-
+	   Error();
+	   Error(const char* errorMessage);
    // destructor
-
+	   ~Error();
    // deleted constructor and operator=
-
+	   Error(const Error& em) = delete;
+	   Error& operator=(const Error& em) = delete;
    // operator= for c-style strings
-
+	   void operator=(const char* errorMessage);
    // methods
-
+	   virtual ~Error();
+	   void clear();
+	   bool isClear()const;
+	   void message(const char* value);
+	   operator const char*() const;
+	   operator bool()const;
    // cast overloads
 
    };
    // operator << overload prototype for cout
+   std::ostream& operator<<(std::ostream& os, const Student& s);
 }
-
-
